8ddbe0f64bbea6e5ded68c9ddaa02297c2408cb4
joshivinit2018 <joshivinit2018@gmailcom>
2021-04-24 22:35:43 +0530

    [vinit]:add  used ShowBoard method to show the current board

--- TicTacToeGame.java
+++ TicTacToeGame.java
@@ -7,7 +7,7 @@ public class TicTacToeGame
 {
     public static void main(String[] args)
    	{
-		        char[] board = createBoard();
+		  char[] board = createBoard();
         char UserLetter = chooseLetter();
         char ComputerLetter;
         if(UserLetter=='X')
@@ -28,6 +28,22 @@ public class TicTacToeGame
         symbol=UserInput.next().toUpperCase().charAt(0);
         return symbol;
     }
+
+		 public static void showBoard(char[] board)
+    {
+        System.out.println("Current Board");
+
+        for (int index = 1; index < board.length; index++)
+        {
+            System.out.print(board[index] + " | ");
+            if(index == 3 || index == 6 || index == 9)
+            {
+                System.out.println();
+            }
+        }
+
+    }
+
     public static char[] createBoard()
     {
         char[] board = new char[10];
